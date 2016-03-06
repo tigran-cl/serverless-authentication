fdf71f73990aed982c2aed2a0c5c95ab54894f32
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-03-06 09:42:08 +0200

    google provider to es6

--- src/providers/google.js
+++ src/providers/google.js
@@ -1,58 +1,53 @@
 'use strict';
 
-var https = require('https');
-var async = require('async');
-var utils = require('../utils');
+import Utils from '../utils';
+import async from 'async';
+import Request from '../request';
 
-module.exports = {
-  signin: function(event, config, callback) {
-    var params = {
-      client_id: config.google.id,
-      redirect_uri: utils.redirectUrlBuilder(event, config),
-      response_type: 'code',
-      scope: 'profile'
-    };
-    var url = utils.urlBuilder('https://accounts.google.com/o/oauth2/v2/auth', params);
-    callback(null, {url: url})
-  },
-  callback: function(event, config, callback) {
-     var params = {
-      client_id: config.google.id,
-      redirect_uri: utils.redirectUrlBuilder(event),
-      client_secret: config.google.secret,
-      code: event.code,
-      grant_type: 'authorization_code'
-    };  
-    async.waterfall([
-      function(callback) {
-        var options = {
-          hostname: 'www.googleapis.com',
-          port: 443,
-          path: '/oauth2/v4/token?'+utils.urlParams(params),
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'}
-        };
-        var req = https.request(options, function(res) {
-          res.on('data', function(d) {
-            var n = JSON.parse(d);
-            callback(null, n);
-          });
-        })
-        req.end();
-        req.on('error', callback);
-      },
-      function(data, callback) {
-        https.get(utils.urlBuilder('https://www.googleapis.com/plus/v1/people/me', {fields: 'id', access_token: data.access_token}), function(res) {
-          res.on('data', function(d) {
-            var n = JSON.parse(d);
-            var result = data;
-            result.client_id = n.id;
-            callback(null, result);
-          });
-        }).on('error', callback);
-      }
-    ], function (err, data) {
-      callback(err, {url: utils.urlBuilder(config.redirect, {token: utils.createToken(data.client_id), client_id: data.client_id})});
+export function signin(event, config, callback) {
+  let params = {
+    client_id: config.google.id,
+    redirect_uri: Utils.redirectUrlBuilder(event, config),
+    response_type: 'code',
+    scope: 'profile'
+  };
+  let url = Utils.urlBuilder('https://accounts.google.com/o/oauth2/v2/auth', params);
+  callback(null, {url: url})
+}
+
+export function callback(event, config, callback) {
+  let params = {
+    client_id: config.google.id,
+    redirect_uri: Utils.redirectUrlBuilder(event, config),
+    client_secret: config.google.secret,
+    code: event.code,
+    grant_type: 'authorization_code'
+  };
+  async.waterfall([
+    (callback) => {
+      (new Request({url: 'https://www.googleapis.com/oauth2/v4/token', params, method: 'POST'})).make(callback);
+    },
+    (data, callback) => {
+
+      let p = {
+        url: 'https://www.googleapis.com/plus/v1/people/me',
+        params: {
+          fields: 'id',
+          access_token: data.access_token
+        }
+      };
+      (new Request(p)).make((err, res) => {
+        let result = data;
+        result.client_id = res.id;
+        callback(null, result);
+      });
+    }
+  ], (err, data) => {
+    callback(err, {
+      url: Utils.urlBuilder(config.redirect, {
+        client_id: data.client_id,
+        token: Utils.createToken(data.client_id, config)
+      })
     });
-  }
+  });
 };
