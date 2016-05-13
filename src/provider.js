5502fe6e817210cc20aeb830d34883eb3003d936
C.J. Winslow <whoaa512@gmail.com>
2016-05-13 13:32:41 -0700

    Remove dependency on async lib and use promises instead

--- src/provider.js
+++ src/provider.js
@@ -1,5 +1,5 @@
 import { Utils } from './utils';
-import async from 'async';
+import Promise from 'any-promise';
 import request from 'request';
 
 /**
@@ -41,43 +41,56 @@ export class Provider {
     additionalParams, cb) {
     const { authorization, profile } = additionalParams;
     const { id, redirect_uri, secret, provider } = this.config;
-    async.waterfall([
-      (callback) => {
-        const mandatoryParams = {
-          client_id: id,
-          redirect_uri,
-          client_secret: secret,
-          code
-        };
-        const payload = Object.assign(mandatoryParams, authorization);
-        if (authorizationMethod === 'GET') {
-          const url = Utils.urlBuilder(authorization_uri, payload);
-          request.get(url, callback);
-        } else {
-          request.post(authorization_uri, { form: payload }, callback);
-        }
-      },
-      (response, accessData, callback) => {
-        if (!accessData) {
-          callback('No access data');
-        }
-        const { access_token } = JSON.parse(accessData);
-        const url = Utils.urlBuilder(profile_uri, Object.assign({ access_token }, profile));
-        request.get(url, (error, httpResponse, profileData) => {
+
+    const attemptAuthorize = () => new Promise((resolve, reject) => {
+      const mandatoryParams = {
+        client_id: id,
+        redirect_uri,
+        client_secret: secret,
+        code
+      };
+      const payload = Object.assign(mandatoryParams, authorization);
+      if (authorizationMethod === 'GET') {
+        const url = Utils.urlBuilder(authorization_uri, payload);
+        request.get(url, (error, response, accessData) => {
           if (error) {
-            callback(error);
-          } else if (!profileData) {
-            callback('No profile data');
-          } else {
-            const profileJson = JSON.parse(profileData);
-            profileJson.provider = provider;
-            const mappedProfile = profileMap ? profileMap(profileJson) : profileJson;
-            callback(null, mappedProfile);
+            return reject(error);
           }
+          return resolve(accessData);
+        });
+      } else {
+        request.post(authorization_uri, { form: payload }, (error, response, accessData) => {
+          if (error) {
+            return reject(error);
+          }
+          return resolve(accessData);
         });
       }
-    ], (err, data) => {
-      cb(err, data, state);
     });
+
+    const createMappedProfile = (accessData) => new Promise((resolve, reject) => {
+      if (!accessData) {
+        reject(new Error('No access data'));
+      }
+      const { access_token } = JSON.parse(accessData);
+      const url = Utils.urlBuilder(profile_uri, Object.assign({ access_token }, profile));
+      request.get(url, (error, httpResponse, profileData) => {
+        if (error) {
+          reject(error);
+        } else if (!profileData) {
+          reject(new Error('No profile data'));
+        } else {
+          const profileJson = JSON.parse(profileData);
+          profileJson.provider = provider;
+          const mappedProfile = profileMap ? profileMap(profileJson) : profileJson;
+          resolve(mappedProfile);
+        }
+      });
+    });
+
+    attemptAuthorize()
+    .then(createMappedProfile)
+    .then((data) => cb(null, data, state))
+    .catch((error) => cb(error));
   }
 }
